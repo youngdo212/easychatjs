0d5b69dd1746dd7d5247cf99b2f31c8c177792bf
mando <nsa06031@gmail.com>
2019-09-09 12:30:34 +0900

    chore: set webpack mode to development

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'messenger.js',
     path: path.resolve(__dirname, 'public/sdk'),
   },
-  // optimization: {
-	// 	minimize: false
-	// },
-}
+};
