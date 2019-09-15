1f2bbd2ef69559e3d7ee6da8a54014a987e3e464
mando <nsa06031@gmail.com>
2019-09-15 19:01:05 +0900

    refactor: modify cors whitelist url

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: 'http://localhost:8080',
   credentials: true,
 }));
 
